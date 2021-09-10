acaf1b95b2b7a49033fc3693d8f0ce7b7d56f80c
longhai0210 <loghai.0210@gmail.com>
2021-09-11 02:21:54 +0700

    add import excel

--- website/src/components/Layout/UI.js
+++ website/src/components/Layout/UI.js
@@ -156,12 +156,12 @@ const UI = (props) => {
           title: 'Quản lý sản phẩm',
           permissions: [PERMISSIONS.quan_li_san_pham],
         },
-        // {
-        //   icon: <BankOutlined />,
-        //   path: ROUTES.INVENTORY,
-        //   title: 'Quản lý kho',
-        //   permissions: [PERMISSIONS.quan_li_kho],
-        // },
+        {
+          icon: <BankOutlined />,
+          path: ROUTES.INVENTORY,
+          title: 'Quản lý kho',
+          permissions: [PERMISSIONS.quan_li_kho],
+        },
         {
           icon: <RotateLeftOutlined />,
           path: ROUTES.SHIPPING_PRODUCT,
@@ -619,8 +619,10 @@ const UI = (props) => {
     }
   }
 
-  const changeBranch = async (value) =>
+  const changeBranch = async (value) => {
     dispatch({ type: 'SET_BRANCH_ID', data: value })
+    updateUser({ branch_id: value }, user.user_id)
+  }
 
   useEffect(() => {
     getInfoUser()
@@ -1008,7 +1010,7 @@ const UI = (props) => {
                   style={{ width: isMobile ? '90%' : 250 }}
                   size="large"
                   onChange={changeBranch}
-                  value={branchId}
+                  value={branchId || user.branch_id}
                 >
                   {listBranch.map((e, index) => (
                     <Option value={e.branch_id} key={index}>
--- website/src/views/guarantee/index.js
+++ website/src/views/guarantee/index.js
@@ -9,14 +9,23 @@ import {
   DatePicker,
   Table,
   notification,
+  Upload,
 } from 'antd'
 import { Link } from 'react-router-dom'
 import { FileExcelOutlined, PlusCircleOutlined } from '@ant-design/icons'
 import moment from 'moment'
-import { apiAllWarranty, updateWarranty } from '../../apis/warranty'
+import {
+  addWarranty,
+  apiAllWarranty,
+  updateWarranty,
+} from '../../apis/warranty'
 import { ROUTES, PERMISSIONS } from 'consts'
 import Permission from 'components/permission'
+import exportToCSV from 'components/ExportCSV/export'
+import ImportModal from 'components/ExportCSV/importModal'
+import { convertFields, guarantee } from 'components/ExportCSV/fieldConvert'
 import { compare } from 'utils'
+import * as XLSX from 'xlsx'
 const { RangePicker } = DatePicker
 function removeFalse(a) {
   return Object.keys(a)
@@ -26,6 +35,9 @@ function removeFalse(a) {
 export default function Guarantee() {
   const [warrantyList, setWarrantyList] = useState([])
   const [pagination, setPagination] = useState({ page: 1, page_size: 10 })
+  const [showImport, setShowImport] = useState(false)
+  const [importData, setImportData] = useState([])
+  const [importLoading, setImportLoading] = useState(false)
   const [filter, setFilter] = useState({
     search: '',
     from_date: undefined,
@@ -113,6 +125,66 @@ export default function Guarantee() {
     },
   ]
 
+  const settings = {
+    name: 'file',
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    headers: {
+      authorization: 'authorization-text',
+    },
+    maxCount: 1,
+    onChange(info) {
+      if (info.file.status !== 'uploading') {
+        info.file.status = 'done'
+
+        setImportLoading(true)
+      }
+      if (info.file.status == 'done') {
+        const reader = new FileReader()
+        reader.onload = async (e) => {
+          const bstr = e.target.result
+          const workBook = XLSX.read(bstr, { type: 'binary' })
+          const workSheetname = workBook.SheetNames[0]
+          const workSheet = workBook.Sheets[workSheetname]
+
+          const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 0 })
+          setImportData(fileData.map((e) => convertFields(e, guarantee)))
+          setImportLoading(false)
+        }
+
+        reader.readAsBinaryString(info.file.originFileObj)
+      }
+    },
+  }
+
+  const handleImport = async () => {
+    console.log(importData)
+    try {
+      const res = await Promise.all(
+        importData.map((e) => {
+          return addWarranty(e)
+        })
+      )
+      if (res.reduce((a, b) => a && b.data.success, true)) {
+        setShowImport(false)
+        setImportData([])
+        getWarranty({ ...removeFalse(filter) })
+        notification.success({ message: 'Import thành công' })
+      } else {
+        res.forEach((e) => {
+          if (!e.data.success) {
+            notification.error({
+              message: 'Thất bại',
+              description: e.data.message,
+            })
+          }
+        })
+      }
+    } catch (err) {
+      console.log(err)
+      notification.error({ message: 'Thất bại' })
+    }
+  }
+
   const changePagi = (page, page_size) => setPagination({ page, page_size })
   const getWarranty = async (params) => {
     try {
@@ -123,6 +195,13 @@ export default function Guarantee() {
     } catch (e) {
       console.log(e)
     }
+  }
+  const ImportComponent = () => {
+    return (
+      <Upload {...settings}>
+        <Button type="primary">Import</Button>
+      </Upload>
+    )
   }
   const onResetFilter = () => {
     setFilter({
@@ -276,6 +355,7 @@ export default function Guarantee() {
                     color: 'white',
                   }}
                   size="large"
+                  onClick={() => setShowImport(true)}
                 >
                   Nhập excel
                 </Button>
@@ -302,6 +382,14 @@ export default function Guarantee() {
                     color: 'white',
                   }}
                   size="large"
+                  onClick={() =>
+                    exportToCSV(
+                      warrantyList.map((e) =>
+                        convertFields(e, guarantee, true)
+                      ),
+                      'bao_hanh'
+                    )
+                  }
                 >
                   Xuất excel
                 </Button>
@@ -323,6 +411,44 @@ export default function Guarantee() {
             pagination={{ onChange: changePagi }}
             dataSource={warrantyList}
             style={{ width: '100%' }}
+          />
+          <ImportModal
+            visible={showImport}
+            importLoading={importLoading}
+            columns={[
+              {
+                title: 'Tên bảo hành',
+                dataIndex: 'name',
+                width: 150,
+                sorter: (a, b) => compare(a, b, 'name'),
+              },
+              {
+                title: 'Loại bảo hành',
+                dataIndex: 'type',
+                width: 150,
+                sorter: (a, b) => compare(a, b, 'type'),
+              },
+              {
+                title: 'Thời hạn bảo hành',
+                dataIndex: 'time',
+                width: 150,
+                render(data) {
+                  return data + ' tháng'
+                },
+                sorter: (a, b) => a.time - b.time,
+              },
+              {
+                title: 'Mô tả',
+                dataIndex: 'description',
+                width: 150,
+                sorter: (a, b) => compare(a, b, 'description'),
+              },
+            ]}
+            downTemplate="./template/guarantee.xlsx"
+            actionComponent={<ImportComponent />}
+            dataSource={importData}
+            onCancel={() => setShowImport(false)}
+            onOk={handleImport}
           />
         </div>
       </div>
