875d777366ce8d1f0f51ca59a7e819753618e3b6
viesoftware <viesoftware2021@gmail.com>
2021-09-27 16:13:55 +0700

    add UI product inventory

---
+++ website/src/views/inventory/columns.js
@@ -0,0 +1,40 @@
+const columnsProduct = [
+  {
+    title: 'Hình ảnh',
+    key: 'image',
+  },
+  {
+    title: 'Tên sản phẩm',
+    dataIndex: 'name',
+    key: 'name-product',
+  },
+  {
+    title: 'SKU',
+    dataIndex: 'sku',
+    key: 'sku',
+  },
+  {
+    title: 'Danh mục',
+    key: 'category',
+  },
+  {
+    title: 'Tổng số lượng',
+    key: 'sum-count',
+  },
+  {
+    title: 'Giá nhập',
+    dataIndex: 'import_price',
+    key: 'import-price',
+  },
+  {
+    title: 'Giá bán',
+    dataIndex: 'sale_price',
+    key: 'sale-price',
+  },
+  {
+    title: 'Mở bán',
+    key: 'active',
+  },
+]
+
+export default columnsProduct
