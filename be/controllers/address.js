7d0908c4acdf9592063b1ebff16846558f4a688e
phandangluu.07061993@gmail.com <viesoftware2021@gmail.com>
2021-11-01 00:10:12 +0700

    fix filter address

--- be/controllers/address.js
+++ be/controllers/address.js
@@ -4,6 +4,8 @@ const crypto = require(`crypto`);
 const client = require(`../config/mongo/mongodb`);
 const DB = process.env.DATABASE;
 const { createTimeline } = require('../utils/date-handle');
+const { removeUnicode } = require('../utils/string-handle');
+const { relative } = require('../utils/filter');
 const { createRegExpQuery } = require('../utils/regex');
 
 let getWardC = async (req, res, next) => {
@@ -16,24 +18,32 @@ let getWardC = async (req, res, next) => {
             matchQuery['ward_code'] = req.query.ward_code;
         }
         if (req.query.district_id) {
-            mongoQuery['district_id'] = parseInt(req.query.district_id);
+            matchQuery['district_id'] = parseInt(req.query.district_id);
         }
         if (req.query.province_id) {
-            mongoQuery['province_id'] = parseInt(req.query.province_id);
-        }
-        // lấy các thuộc tính tìm kiếm với độ chính xác tương đối ('1' == '1', '1' == '12',...)
-        if (req.query.ward_name) {
-            matchQuery['ward_name'] = createRegExpQuery(req.query.ward_name);
-        }
-        if (req.query.district_name) {
-            matchQuery['district_name'] = createRegExpQuery(req.query.district_name);
-        }
-        if (req.query.province_name) {
-            matchQuery['province_name'] = createRegExpQuery(req.query.province_name);
+            matchQuery['province_id'] = parseInt(req.query.province_id);
         }
         aggregateQuery.push({ $match: matchQuery });
         // lấy data từ database
         let wards = await client.db(DB).collection(`Wards`).aggregate(aggregateQuery).toArray();
+        wards = relative(
+            {
+                ...(() => {
+                    let result = {};
+                    if (req.query.ward_name) {
+                        result = { ...result, ...{ ward_name: req.query.ward_name } };
+                    }
+                    if (req.query.district_name) {
+                        result = { ...result, ...{ district_name: req.query.district_name } };
+                    }
+                    if (req.query.province_name) {
+                        result = { ...result, ...{ province_name: req.query.province_name } };
+                    }
+                    return result;
+                })(),
+            },
+            wards
+        );
         res.send({ success: true, data: wards });
     } catch (err) {
         next(err);
@@ -47,21 +57,29 @@ let getDistrictC = async (req, res, next) => {
         let aggregateQuery = [];
         // lấy các thuộc tính tìm kiếm cần độ chính xác cao ('1' == '1', '1' != '12',...)
         if (req.query.district_id) {
-            mongoQuery['district_id'] = parseInt(req.query.district_id);
+            matchQuery['district_id'] = parseInt(req.query.district_id);
         }
         if (req.query.province_id) {
-            mongoQuery['province_id'] = parseInt(req.query.province_id);
-        }
-        // lấy các thuộc tính tìm kiếm với độ chính xác tương đối ('1' == '1', '1' == '12',...)
-        if (req.query.district_name) {
-            matchQuery['district_name'] = createRegExpQuery(req.query.district_name);
-        }
-        if (req.query.province_name) {
-            matchQuery['province_name'] = createRegExpQuery(req.query.province_name);
+            matchQuery['province_id'] = parseInt(req.query.province_id);
         }
         aggregateQuery.push({ $match: matchQuery });
         // lấy data từ database
         let districts = await client.db(DB).collection(`Districts`).aggregate(aggregateQuery).toArray();
+        districts = relative(
+            {
+                ...(() => {
+                    let result = {};
+                    if (req.query.district_name) {
+                        result = { ...result, ...{ district_name: req.query.district_name } };
+                    }
+                    if (req.query.province_name) {
+                        result = { ...result, ...{ province_name: req.query.province_name } };
+                    }
+                    return result;
+                })(),
+            },
+            districts
+        );
         res.send({ success: true, data: districts });
     } catch (err) {
         console.log(err);
@@ -75,22 +93,24 @@ let getProvinceC = async (req, res, next) => {
         let projectQuery = {};
         let aggregateQuery = [];
         // lấy các thuộc tính tìm kiếm cần độ chính xác cao ('1' == '1', '1' != '12',...)
-        if (req.query.district_id) {
-            mongoQuery['district_id'] = parseInt(req.query.district_id);
-        }
         if (req.query.province_id) {
-            mongoQuery['province_id'] = parseInt(req.query.province_id);
-        }
-        // lấy các thuộc tính tìm kiếm với độ chính xác tương đối ('1' == '1', '1' == '12',...)
-        if (req.query.district_name) {
-            matchQuery['district_name'] = createRegExpQuery(req.query.district_name);
-        }
-        if (req.query.province_name) {
-            matchQuery['province_name'] = createRegExpQuery(req.query.province_name);
+            matchQuery['province_id'] = parseInt(req.query.province_id);
         }
         aggregateQuery.push({ $match: matchQuery });
         // lấy data từ database
         let provinces = await client.db(DB).collection(`Provinces`).aggregate(aggregateQuery).toArray();
+        provinces = relative(
+            {
+                ...(() => {
+                    let result = {};
+                    if (req.query.province_name) {
+                        result = { ...result, ...{ province_name: req.query.province_name } };
+                    }
+                    return result;
+                })(),
+            },
+            provinces
+        );
         res.send({ success: true, data: provinces });
     } catch (err) {
         next(err);
