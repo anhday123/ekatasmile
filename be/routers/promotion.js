e321b214235d496de61d65c00594aed92116e2cb
phandangluu.07061993@gmail.com <viesoftware2021@gmail.com>
2021-11-08 17:26:57 +0700

    voucher

--- be/routers/promotion.js
+++ be/routers/promotion.js
@@ -7,6 +7,6 @@ const { auth } = require(`../middleware/jwt`);
 router.route(`/getpromotion`).get(auth, promotion.getPromotionC);
 router.route(`/addpromotion`).post(auth, promotion.addPromotionC);
 router.route(`/updatepromotion/:promotion_id`).patch(auth, promotion.updatePromotionC);
-router.route(`/usevoucher`).post(auth, promotion.checkVoucherC);
+router.route(`/checkvoucher`).post(auth, promotion.checkVoucherC);
 
 module.exports = router;
