0348671e5438172eb8df8bab86a58c5b4ac2b04f
Vie Team <viesoftware2021@gmail.com>
2021-11-12 13:28:25 +0700

    add endpoint feedback

--- be/routers/product.js
+++ be/routers/product.js
@@ -8,5 +8,6 @@ router.route(`/getproduct`).get(auth, product.getProductC);
 router.route(`/addproduct`).post(auth, product.addProductC);
 router.route(`/updateproduct/:product_id`).patch(auth, product.updateProductC);
 router.route(`/getattribute`).get(auth, product.getAllAtttributeC);
+router.route(`/addfeddback`).get(auth, product.addFeedbackC);
 
 module.exports = router;
