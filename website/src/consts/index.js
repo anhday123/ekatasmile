af47a8fa121ceca645d0e6bf822d8feae16855c7
viesoftware <viesoftware2021@gmail.com>
2021-11-26 22:52:37 +0700

    up version deploy

--- website/src/consts/index.js
+++ website/src/consts/index.js
@@ -193,7 +193,7 @@ export const PERMISSIONS = {
   tao_kenh_ban_hang:'tao_kenh_ban_hang',
 }
 
-export const VERSION_APP = '1.0.5'
+export const VERSION_APP = '1.0.6'
 
 export const regexPhone = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/
 
