29221522c42e2375e0a7d547f648a9d26bc8e9dc
Vie Team <81791877+viesoftware@users.noreply.github.com>
2021-12-09 11:21:35 +0700

    Update warranty.js

--- be/services/warranty.js
+++ be/services/warranty.js
@@ -35,30 +35,21 @@ let getWarrantyS = async (req, res, next) => {
         if (req.query.code) {
             aggregateQuery.push({
                 $match: {
-                    code: new RegExp(
-                        `${removeUnicode(req.query.code, false).replace(/(\s){1,}/g, '(.*?)')}`,
-                        'ig'
-                    ),
+                    code: new RegExp(`${removeUnicode(req.query.code, false).replace(/(\s){1,}/g, '(.*?)')}`, 'ig'),
                 },
             });
         }
         if (req.query.name) {
             aggregateQuery.push({
                 $match: {
-                    sub_name: new RegExp(
-                        `${removeUnicode(req.query.name, false).replace(/(\s){1,}/g, '(.*?)')}`,
-                        'ig'
-                    ),
+                    sub_name: new RegExp(`${removeUnicode(req.query.name, false).replace(/(\s){1,}/g, '(.*?)')}`, 'ig'),
                 },
             });
         }
         if (req.query.type) {
             aggregateQuery.push({
                 $match: {
-                    sub_type: new RegExp(
-                        `${removeUnicode(req.query.type, false).replace(/(\s){1,}/g, '(.*?)')}`,
-                        'ig'
-                    ),
+                    sub_type: new RegExp(`${removeUnicode(req.query.type, false).replace(/(\s){1,}/g, '(.*?)')}`, 'ig'),
                 },
             });
         }
@@ -91,7 +82,7 @@ let getWarrantyS = async (req, res, next) => {
                     $lookup: {
                         from: 'Users',
                         localField: 'business_id',
-                        foreignField: '_id',
+                        foreignField: 'user_id',
                         as: '_business',
                     },
                 },
@@ -104,7 +95,7 @@ let getWarrantyS = async (req, res, next) => {
                     $lookup: {
                         from: 'Users',
                         localField: 'creator_id',
-                        foreignField: '_id',
+                        foreignField: 'user_id',
                         as: '_creator',
                     },
                 },
