b513786685ef23c022a9ce542b6dfe66dc0323ab
Vie Team <81791877+viesoftware@users.noreply.github.com>
2021-12-10 18:10:32 +0700

    update

--- be/test6.js
+++ be/test6.js
@@ -1,5 +1,5 @@
-(async () => {
-    if (true) {
-        console.log(1);
-    }
-})();
+let obj1 = { a: 1, b: 2, c: 3 };
+let obj2 = { b: 5, d: 5 };
+obj1 = { ...obj1, ...obj2 };
+console.log(obj1);
+
