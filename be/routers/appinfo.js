78fb6874f735feb1a4f5fdcfa6467bbecfc4921b
Vie Team <81791877+viesoftware@users.noreply.github.com>
2022-01-08 23:34:50 +0700

    Update appinfo.js

--- be/routers/appinfo.js
+++ be/routers/appinfo.js
@@ -6,6 +6,6 @@ const { auth } = require(`../middleware/jwt`);
 
 router.route(`/`).get(auth, appInfo._getAppInfo);
 router.route(`/update`).patch(auth, appInfo._updateAppInfo);
-router.route(`/checkdomain`).post(auth, appInfo._checkDomain);
+router.route(`/checkdomain`).post(appInfo._checkDomain);
 
 module.exports = router;
