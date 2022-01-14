5d257af3d6791f633c0714a2ab2cba25f5a15bd8
viesoftware <viesoftware2021@gmail.com>
2022-01-14 19:53:03 +0700

    merge code branch duc and deploy

--- website/src/views/login/index.js
+++ website/src/views/login/index.js
@@ -28,7 +28,7 @@ export default function Login() {
       subDomain = subDomain[0].split('//')
 
       //code xong chỉnh lại như cũ
-      // const res = await login({ ...body, username: `0814790913_${body.username}` })
+      // const res = await login({ ...body, username: `hoang_${body.username}` })
       const res = await login({ ...body, username: `${subDomain[1]}_${body.username}` })
 
       dispatch({ type: ACTION.LOADING, data: false })
