e9a203aef5977ac99141b480666b55c76f5c46d8
Vie Team <81791877+viesoftware@users.noreply.github.com>
2022-01-14 16:33:25 +0700

    Update business.js

--- be/routers/business.js
+++ be/routers/business.js
@@ -5,7 +5,7 @@ const business = require(`../controllers/business`);
 const { auth } = require(`../middleware/jwt`);
 
 // router.route(`/create`).post(auth, business._create);
-// router.route(`/update/:business_id`).patch(auth, business._update);
+router.route(`/update/:business_id`).patch(auth, business._update);
 router.route(`/`).get(auth, business._get);
 router.route(`/delete`).delete(auth, business._delete);
 
