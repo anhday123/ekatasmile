96f2d18770f26051a06b0326b5b85a08559a7a0a
mandeveloper2k <81791877+viesoftware@users.noreply.github.com>
2022-01-16 13:42:29 +0700

    add enum

---
+++ be/models/status_code.js
@@ -0,0 +1,10 @@
+class StatusCode {
+     data = {
+        200:'get success',
+        201:'create/update/delete success',
+        401:'invalid param',
+        404:'not found models',
+        500:'server busy'
+    }
+
+}
--- be/routers/order.js
+++ be/routers/order.js
@@ -7,6 +7,8 @@ const { auth } = require(`../middleware/jwt`);
 router.route(`/create`).post(auth, order._create);
 router.route(`/update/:order_id`).patch(auth, order._update);
 router.route(`/`).get(auth, order._get);
+router.route('/status').get(auth, order.enumStatusOrder);
+router.route('/status/shipping').get(auth, order.enumStatusShipping);
 router.route('/delete').delete(auth, order._delete);
 
 module.exports = router;
