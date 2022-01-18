39a299b79a71e856acd2bac1c1103ce814d9afcb
Vie Team <81791877+viesoftware@users.noreply.github.com>
2022-01-18 16:13:37 +0700

    Update enum.js

--- be/routers/enum.js
+++ be/routers/enum.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const _enum = require(`../controllers/enum`);
 const { auth } = require(`../middleware/jwt`);
 
-router.route(`/order`).get(auth, _enum._getEnumOrderStatus);
-router.route(`/payment`).get(auth, _enum._getEnumPaymentStatus);
-router.route(`/shipping`).get(auth, _enum._getEnumShippingStatus);
-router.route(`/importorder`).get(auth, _enum._getEnumImportOrderStatus);
-router.route(`/transportorder`).get(auth, _enum._getEnumTransportOrderStatus);
+router.route(`/order`).get(_enum._getEnumOrderStatus);
+router.route(`/payment`).get(_enum._getEnumPaymentStatus);
+router.route(`/shipping`).get(_enum._getEnumShippingStatus);
+router.route(`/importorder`).get(_enum._getEnumImportOrderStatus);
+router.route(`/transportorder`).get(_enum._getEnumTransportOrderStatus);
 
 module.exports = router;
