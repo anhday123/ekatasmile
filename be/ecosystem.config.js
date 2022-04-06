36b6dd82ff91dc5b2f6133b4a7140ef2134344ee
Vie Team <81791877+viesoftware@users.noreply.github.com>
2022-04-07 01:08:17 +0700

    Update ecosystem.config.js

--- be/ecosystem.config.js
+++ be/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
             ref: 'origin/master',
             repo: 'git@github.com:viesoftware/System_Admin_Order.git',
             path: '/root/System_Admin_Order',
-            'post-deploy': 'cd ./source/be && npm install && pm2 reload ecosystem.config.js',
+            'post-deploy': 'cd /root/System_Admin_Order/source/be && npm install && pm2 reload ecosystem.config.js',
         },
     },
 };
