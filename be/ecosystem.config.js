cd78756e4534c64719f73a9e9f3cb8d60ad48a83
Vie Team <81791877+viesoftware@users.noreply.github.com>
2022-04-07 02:41:05 +0700

    update

--- be/ecosystem.config.js
+++ be/ecosystem.config.js
@@ -7,10 +7,22 @@ module.exports = {
         },
     ],
     deploy: {
-        production: {
-            key: '~/.ssh/id_rsa',
+        sandbox: {
+            key: './key.pem',
             user: 'root',
             host: ['103.81.87.65'],
+            ssh_options: 'StrictHostKeyChecking=no',
+            ref: 'origin/master',
+            repo: 'git@github.com:viesoftware/System_Admin_Order.git',
+            path: '/root/System_Admin_Order',
+            'post-deploy':
+                'cd /root/System_Admin_Order/source/be && npm install && pm2 reload ecosystem.config.js --env sandbox',
+        },
+        production: {
+            key: '/root/',
+            user: 'root',
+            host: ['103.81.87.65'],
+            ssh_options: 'StrictHostKeyChecking=no',
             ref: 'origin/master',
             repo: 'git@github.com:viesoftware/System_Admin_Order.git',
             path: '/root/System_Admin_Order',
