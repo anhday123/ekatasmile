0ce4c9eba501d17fc8b2663539daa1f4da5c7cb5
Võ Đại Gia <86181153+vdgoiaa@users.noreply.github.com>
2022-04-07 17:42:43 +0700

    refactor UI shipping

--- website/src/views/shipping/index.js
+++ website/src/views/shipping/index.js
@@ -1,43 +1,25 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { ACTION, PERMISSIONS, ROUTES } from 'consts'
-import moment from 'moment'
-import { useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom'
-
-//antd
-import {
-  notification,
-  Input,
-  Button,
-  Row,
-  Col,
-  Table,
-  Select,
-  DatePicker,
-  Space,
-  Popconfirm,
-} from 'antd'
-
+import styles from './shipping.module.scss'
 // icons
-import { ArrowLeftOutlined, PlusCircleOutlined, SearchOutlined } from '@ant-design/icons'
-
+import { ArrowLeftOutlined, PlusCircleOutlined } from '@ant-design/icons'
+//antd
+import { Button, Col, Form, Input, Modal, notification, Row, Select, Space } from 'antd'
+import { getDistricts, getProvinces } from 'apis/address'
 //apis
-import { getShippings, deleteShippings } from 'apis/shipping'
-import { getProvinces, getDistricts } from 'apis/address'
-
+import { deleteShippings, getShippings } from 'apis/shipping'
 //components
 import Permission from 'components/permission'
 import TitlePage from 'components/title-page'
-import columnsShipping from './columns'
-import SettingColumns from 'components/setting-columns'
-import ShippingForm from './shipping-form'
+import { PERMISSIONS, ROUTES } from 'consts'
+import React, { useEffect, useRef, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
 const { Option } = Select
 export default function Shipping() {
   const dispatch = useDispatch()
   const history = useHistory()
   const typingTimeoutRef = useRef(null)
-
+  const [form] = Form.useForm()
   const [loading, setLoading] = useState(false)
   const [countShipping, setCountShipping] = useState(0)
   const [shippings, setShippings] = useState([])
@@ -47,12 +29,18 @@ export default function Shipping() {
   const [provinces, setProvinces] = useState([])
   const [valueSearch, setValueSearch] = useState('')
   const [paramsFilter, setParamsFilter] = useState({ page: 1, page_size: 20 })
-
+  const [modalVisible, setModalVisible] = useState(false)
+  const [shippingCompanyName, setShippingCompanyName] = useState('')
   const [isOpenSelect, setIsOpenSelect] = useState(false)
   const toggleOpenSelect = () => setIsOpenSelect(!isOpenSelect)
   const [valueTime, setValueTime] = useState() //dùng để hiện thị value trong filter by time
   const [valueDateTimeSearch, setValueDateTimeSearch] = useState({})
   const [valueDateSearch, setValueDateSearch] = useState(null) //dùng để hiện thị date trong filter by date
+
+  const toggleModal = (name) => {
+    setShippingCompanyName(name)
+    setModalVisible(!modalVisible)
+  }
 
   const _onFilter = (attribute = '', value = '') => {
     if (value) paramsFilter[attribute] = value
@@ -161,6 +149,50 @@ export default function Shipping() {
   return (
     <>
       <div className="card">
+        <Modal
+          visible={modalVisible}
+          title={`Kết nối đối tác ${shippingCompanyName}`}
+          onCancel={toggleModal}
+          footer={[
+            <Button type="primary" style={{ backgroundColor: '#6D24CF', border: 'none' }}>
+              Kết nối
+            </Button>,
+          ]}
+        >
+          <Form form={form} layout="vertical">
+            {shippingCompanyName === 'GHTK' ? (
+              <>
+                <Form.Item label="Secret key" name="secret_key">
+                  <Input placeholder="Secret key" />
+                </Form.Item>
+                <Form.Item label="Customer code secret key" name="customer_secret_key">
+                  <Input placeholder="Customer code secret key" />
+                </Form.Item>
+                <div className={styles['content-modal']}>
+                  <h4>Hướng dẫn lấy bằng hình ảnh</h4>
+                  <div>
+                    <p>Step 1: Lorem ipsum dolor sit amet, consetetur sadipscing elitr sed diam.</p>
+                    <img
+                      style={{ display: 'block', width: '100%' }}
+                      src="https://s3.ap-northeast-1.wasabisys.com/admin-order/2022/04/07/10e585da-4952-41d0-b92a-e385c3f9a4a3/Group 14113.png"
+                    />
+                  </div>
+                  <div>
+                    <p>Step 2: Lorem ipsum dolor sit amet, consetetur sadipscing elitr sed diam.</p>
+                    <img
+                      style={{ display: 'block', width: '100%' }}
+                      src="https://s3.ap-northeast-1.wasabisys.com/admin-order/2022/04/07/10e585da-4952-41d0-b92a-e385c3f9a4a3/Group 14113.png"
+                    />
+                  </div>
+                </div>
+              </>
+            ) : (
+              <Form.Item label="Token API" name="token_api">
+                <Input placeholder="Token API" />
+              </Form.Item>
+            )}
+          </Form>
+        </Modal>
         <TitlePage
           title={
             <Row
@@ -175,282 +207,121 @@ export default function Shipping() {
           }
         >
           <Space>
-            <SettingColumns
+            {/* <SettingColumns
               nameColumn="columnsShipping"
               columns={columns}
               setColumns={setColumns}
               columnsDefault={columnsShipping}
             />
-            <ShippingForm reloadData={_getShippings}>
-              <Permission permissions={[PERMISSIONS.them_doi_tac_van_chuyen]}>
-                <Button icon={<PlusCircleOutlined />} type="primary" size="large">
-                  Thêm đối tác
-                </Button>
-              </Permission>
-            </ShippingForm>
+            <ShippingForm reloadData={_getShippings}></ShippingForm> */}
+            <Permission permissions={[PERMISSIONS.them_doi_tac_van_chuyen]}>
+              <Button icon={<PlusCircleOutlined />} type="primary" size="large">
+                Thêm đối tác
+              </Button>
+            </Permission>
           </Space>
         </TitlePage>
-
-        <Row gutter={[16, 16]} style={{
-          marginTop: 10,
-          marginRight: 0,
-          marginBottom: 0,
-          marginLeft: 0,
-          border: '1px solid #d9d9d9',
-          borderRadius: 5
-        }}>
-          <Col xs={24} sm={24} md={12} lg={6} xl={6}>
-            <Input
-              prefix={<SearchOutlined />}
-              style={{ width: '100%' }}
-              value={valueSearch}
-              enterButton
-              onChange={onSearch}
-              placeholder="Tìm kiếm theo tên vận chuyển"
-              allowClear
-              bordered={false}
-            />
-          </Col>
-          <Col xs={24} sm={24} md={12} lg={6} xl={6} style={{ borderRight: '1px solid #d9d9d9', borderLeft: '1px solid #d9d9d9' }}>
-            <Select
-              allowClear
-              showSearch
-              style={{ width: '100%' }}
-              placeholder="Lọc theo tỉnh/thành phố"
-              optionFilterProp="children"
-              filterOption={(input, option) =>
-                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              }
-              value={paramsFilter.province}
-              onChange={(value) => _onFilter('province', value)}
-              bordered={false}
-            >
-              {provinces.map((values, index) => {
-                return <Option value={values.province_name}>{values.province_name}</Option>
-              })}
-            </Select>
-          </Col>
-          <Col xs={24} sm={24} md={12} lg={6} xl={6} style={{ borderRight: '1px solid #d9d9d9' }}>
-            <Select
-              allowClear
-              showSearch
-              style={{ width: '100%' }}
-              placeholder="Lọc theo quận/huyện"
-              optionFilterProp="children"
-              filterOption={(input, option) =>
-                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              }
-              value={paramsFilter.district}
-              onChange={(value) => _onFilter('district', value)}
-              bordered={false}
-            >
-              {districts.map((values, index) => {
-                return (
-                  <Option value={values.district_name} key={index}>
-                    {values.district_name}
-                  </Option>
-                )
-              })}
-            </Select>
-          </Col>
-          <Col xs={24} sm={24} md={12} lg={6} xl={6}>
-            <Select
-              open={isOpenSelect}
-              onBlur={() => {
-                if (isOpenSelect) toggleOpenSelect()
+        <Row gutter={[20, 20]} style={{ padding: '20px 10px' }} justify="space-between">
+          <Col span={12}>
+            <Row
+              justify="space-between"
+              align="middle"
+              style={{
+                boxShadow: '0px 4px 25px rgba(37, 107, 254, 0.25)',
+                padding: 20,
+                borderRadius: 10,
               }}
-              onClick={() => {
-                if (!isOpenSelect) toggleOpenSelect()
-              }}
-              allowClear
-              showSearch
-              style={{ width: '100%' }}
-              placeholder="Lọc theo thời gian"
-              optionFilterProp="children"
-              bordered={false}
-              filterOption={(input, option) =>
-                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-              }
-              value={valueTime}
-              onChange={async (value) => {
-                setValueTime(value)
-
-                paramsFilter.page = 1
-
-                //xoa params search date hien tai
-                const p = Object.keys(valueDateTimeSearch)
-                if (p.length) delete paramsFilter[p[0]]
-
-                setValueDateSearch(null)
-                delete paramsFilter.from_date
-                delete paramsFilter.to_date
-
-                if (isOpenSelect) toggleOpenSelect()
-
-                if (value) {
-                  const searchDate = Object.fromEntries([[value, true]]) // them params search date moi
-
-                  setParamsFilter({ ...paramsFilter, ...searchDate })
-                  setValueDateTimeSearch({ ...searchDate })
-                } else {
-                  setParamsFilter({ ...paramsFilter })
-                  setValueDateTimeSearch({})
-                }
-              }}
-              dropdownRender={(menu) => (
-                <>
-                  <DatePicker.RangePicker
-                    onFocus={() => {
-                      if (!isOpenSelect) toggleOpenSelect()
-                    }}
-                    onBlur={() => {
-                      if (isOpenSelect) toggleOpenSelect()
-                    }}
-                    value={valueDateSearch}
-                    onChange={(dates, dateStrings) => {
-                      //khi search hoac filter thi reset page ve 1
-                      paramsFilter.page = 1
-
-                      if (isOpenSelect) toggleOpenSelect()
-
-                      //nếu search date thì xoá các params date
-                      delete paramsFilter.to_day
-                      delete paramsFilter.yesterday
-                      delete paramsFilter.this_week
-                      delete paramsFilter.last_week
-                      delete paramsFilter.last_month
-                      delete paramsFilter.this_month
-                      delete paramsFilter.this_year
-                      delete paramsFilter.last_year
-
-                      //Kiểm tra xem date có được chọn ko
-                      //Nếu ko thì thoát khỏi hàm, tránh cash app
-                      //và get danh sách order
-                      if (!dateStrings[0] && !dateStrings[1]) {
-                        delete paramsFilter.from_date
-                        delete paramsFilter.to_date
-
-                        setValueDateSearch(null)
-                        setValueTime()
-                      } else {
-                        const dateFirst = dateStrings[0]
-                        const dateLast = dateStrings[1]
-                        setValueDateSearch(dates)
-                        setValueTime(`${dateFirst} -> ${dateLast}`)
-
-                        dateFirst.replace(/-/g, '/')
-                        dateLast.replace(/-/g, '/')
-
-                        paramsFilter.from_date = dateFirst
-                        paramsFilter.to_date = dateLast
-                      }
-
-                      setParamsFilter({ ...paramsFilter })
-                    }}
-                    style={{ width: '100%' }}
-                  />
-                  {menu}
-                </>
-              )}
             >
-              <Option value="today">Hôm nay</Option>
-              <Option value="yesterday">Hôm qua</Option>
-              <Option value="this_week">Tuần này</Option>
-              <Option value="last_week">Tuần trước</Option>
-              <Option value="this_month">Tháng này</Option>
-              <Option value="last_month">Tháng trước</Option>
-              <Option value="this_year">Năm này</Option>
-              <Option value="last_year">Năm trước</Option>
-            </Select>
+              <Col span={5}>
+                <img
+                  style={{ display: 'block', width: '80px', height: 80 }}
+                  src="https://s3.ap-northeast-1.wasabisys.com/admin-order/2022/04/07/31d98110-7516-4558-bdf8-6766b891b4e6/Frame 16671.png"
+                />
+                <p>Tự giao hàng</p>
+              </Col>
+              <Col span={4}>
+                <Button type="primary" style={{ backgroundColor: '#68D69D', border: 'none' }}>
+                  Đã kích hoạt
+                </Button>
+              </Col>
+              <Col span={24}>
+                <p>
+                  Nhà bán hàng được chủ động giao sản phẩm và dịch vụ đến người mua mà không cần
+                  thông qua các đối tác vận chuyển nào.
+                </p>
+              </Col>
+            </Row>
+          </Col>
+          <Col span={12}>
+            <Row
+              justify="space-between"
+              align="middle"
+              style={{
+                boxShadow: '0px 4px 25px rgba(37, 107, 254, 0.25)',
+                padding: 20,
+                borderRadius: 10,
+              }}
+            >
+              <Col span={5}>
+                <img
+                  style={{ display: 'block', width: '100px', height: 80 }}
+                  src="https://s3.ap-northeast-1.wasabisys.com/admin-order/2022/04/07/15c0738f-7d3d-46e0-8ca5-0db90617e3b5/ghn.jpg"
+                />
+                <p>Giao hàng nhanh</p>
+              </Col>
+              <Col span={4}>
+                <Button
+                  onClick={() => toggleModal('GHN')}
+                  type="primary"
+                  style={{ backgroundColor: '#FF9240', border: 'none' }}
+                >
+                  Chưa kích hoạt
+                </Button>
+              </Col>
+              <Col span={24}>
+                <p>
+                  Nhà bán hàng được chủ động giao sản phẩm và dịch vụ đến người mua mà không cần
+                  thông qua các đối tác vận chuyển nào.
+                </p>
+              </Col>
+            </Row>
+          </Col>
+          <Col span={12}>
+            <Row
+              justify="space-between"
+              align="middle"
+              style={{
+                boxShadow: '0px 4px 25px rgba(37, 107, 254, 0.25)',
+                padding: 20,
+                borderRadius: 10,
+              }}
+            >
+              <Col span={8}>
+                <img
+                  style={{ display: 'block', width: '100px', height: 80 }}
+                  src="https://s3.ap-northeast-1.wasabisys.com/admin-order/2022/04/07/e6268e25-7ca7-4c03-a486-8c4aded667b8/giao_hang_tiet_kiem.png"
+                />
+                <p>Giao hàng tiết kiệm (chưa kích hoạt)</p>
+              </Col>
+              <Col span={4}>
+                <Button
+                  type="primary"
+                  onClick={() => toggleModal('GHTK')}
+                  style={{ backgroundColor: '#FF9240', border: 'none' }}
+                >
+                  Chưa kích hoạt
+                </Button>
+              </Col>
+              <Col span={24}>
+                <p>
+                  Cung cấp dịch vụ giao hàng tận nơi và thu tiền tận nơi cho các Shop/Doanh nghiệp
+                  kinh doanh trực tuyến. Giao hàng với tốc độ nhanh, mạng lưới phủ sóng rộng trên
+                  toàn quốc.
+                </p>
+              </Col>
+            </Row>
           </Col>
         </Row>
-
-        <Row justify="space-between" wrap={false} style={{ marginBottom: 8, marginTop: 10 }}>
-          <Popconfirm
-            okText="Đồng ý"
-            cancelText="Từ chối"
-            onConfirm={_deleteShippings}
-            title="Bạn có muốn xóa các đối tác này không ?"
-          >
-            {/* <Permission permissions={[PERMISSIONS.xoa_doi_tac_van_chuyen]}> */}
-            <Button
-              size="large"
-              type="primary"
-              danger
-              style={{ visibility: !selectedRowKeys.length && 'hidden' }}
-            >
-              Xóa đối tác
-            </Button>
-            {/* </Permission> */}
-          </Popconfirm>
-
-          <Button
-            style={{ display: Object.keys(paramsFilter).length <= 2 && 'none' }}
-            onClick={_clearFilters}
-            type="primary"
-          >
-            Xóa bộ lọc
-          </Button>
-        </Row>
-
-        <Table
-          size="small"
-          rowKey="shipping_company_id"
-          loading={loading}
-          rowSelection={rowSelection}
-          columns={columns.map((column) => {
-            if (column.key === 'stt')
-              return {
-                ...column,
-                width: 50,
-                render: (text, record, index) =>
-                  (paramsFilter.page - 1) * paramsFilter.page_size + index + 1,
-              }
-            if (column.key === 'code')
-              return {
-                ...column,
-                render: (text, record) => (
-                  <ShippingForm record={record} reloadData={_getShippings}>
-                    <a>{record.code}</a>
-                  </ShippingForm>
-                ),
-              }
-            if (column.key === 'create_date')
-              return {
-                ...column,
-                render: (text, record) =>
-                  record.create_date && moment(record.create_date).format('DD-MM-YYYY HH:mm'),
-              }
-            if (column.key === 'address')
-              return {
-                ...column,
-                render: (text, record) =>
-                  `
-                  ${record.address && record.address + ', '}
-                  ${record.district && record.district + ', '}
-                  ${record.province && record.province + ', '}
-                  ${record.ward && record.ward}
-                  `,
-              }
-
-            return column
-          })}
-          dataSource={shippings}
-          scroll={{ y: 500, width: '100%', marginTop: 10 }}
-          pagination={{
-            position: ['bottomLeft'],
-            current: paramsFilter.page,
-            pageSize: paramsFilter.page_size,
-            pageSizeOptions: [20, 30, 40, 50, 60, 70, 80, 90, 100],
-            showQuickJumper: true,
-            onChange: (page, pageSize) => {
-              paramsFilter.page = page
-              paramsFilter.page_size = pageSize
-              setParamsFilter({ ...paramsFilter })
-            },
-            total: countShipping,
-          }}
-        />
       </div>
     </>
   )
