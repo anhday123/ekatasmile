a714b106146f97ac0c439e717f840def93dd61b2
Vie Team <81791877+viesoftware@users.noreply.github.com>
2022-04-18 16:13:43 +0700

    update

--- be/routers/action.js
+++ be/routers/action.js
@@ -5,5 +5,7 @@ const action = require(`../controllers/action`);
 const { auth } = require(`../middleware/jwt`);
 
 router.route(`/`).get(auth, action._get);
+router.route(`/file-history`).get(auth, action._getFileHistory);
+router.route(`/file-history/create`).post(auth, action._createFileHistory);
 
 module.exports = router;
