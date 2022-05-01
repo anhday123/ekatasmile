d6d38b7d01fc91e9f4266e6eadf59acd8ced2c33
Vie Team <81791877+viesoftware@users.noreply.github.com>
2022-05-01 15:26:22 +0700

    bo sung api action

--- be/routers/action.js
+++ be/routers/action.js
@@ -6,6 +6,7 @@ const { auth } = require(`../middleware/jwt`);
 
 router.route(`/`).get(auth, action._get);
 router.route(`/file-history`).get(auth, action._getFileHistory);
+router.route('/menu-system').get(action.getAllMenuSystem);
 router.route(`/file-history/create`).post(auth, action._createFileHistory);
 
 module.exports = router;
